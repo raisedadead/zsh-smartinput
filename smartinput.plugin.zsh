eee469444317656c519d796a7f09e14e440b5805
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-04-02 13:40:04 +0530

    fix: bugs in brackets and single quotes

--- smartinput.plugin.zsh
+++ smartinput.plugin.zsh
@@ -8,24 +8,22 @@ declare -g __smartinput_rules=(
 )
 
 function __smartinput:widget:input_left_bracket {
-  if [[ "$LBUFFER[-1]" == '\' ]]; then
-    # Support Escape "\"
-    :
-  else
-    local rule
-    for rule in "${__smartinput_rules[@]}"; do
-      if [[ "${#rule}" == 4 && "${rule:0:1}" == "$KEYS" ]]; then
-        RBUFFER="${rule:2:1}$RBUFFER"
-        break
-      fi
-    done
-  fi
+  case "$KEYS" in
+  "[")
+    RBUFFER="]$RBUFFER"
+    ;;
+  "{")
+    RBUFFER="}$RBUFFER"
+    ;;
+  "(")
+    RBUFFER=")$RBUFFER"
+    ;;
+  esac
   zle self-insert
 }
 
 function __smartinput:widget:input_right_bracket {
   if [[ "$RBUFFER[1]" == "$KEYS" ]]; then
-    # [xxxxxx|]<right bracket key>
     zle forward-char
     return
   fi
@@ -33,16 +31,13 @@ function __smartinput:widget:input_right_bracket {
 }
 
 function __smartinput:widget:input_quote {
-  if [[ "$LBUFFER[-1]" == '\' ]]; then
-    # Support Escape "\"
-    :
+  if [[ "$LBUFFER[-1]" == '\\' ]]; then
+    return
   elif [[ "$RBUFFER[1]" == "$KEYS" ]]; then
-    # 'xxxxxx|'<quote key>
     zle forward-char
     return
-  elif [[ "$KEYS" == "'" && "$LBUFFER" =~ '[[:word:]]$' ]]; then
-    # Support English "someone's"
-    :
+  elif [[ "$KEYS" == "'" && "$LBUFFER" =~ [[:alnum:]]$ ]]; then
+    return
   else
     RBUFFER="$KEYS$RBUFFER"
   fi
@@ -51,7 +46,6 @@ function __smartinput:widget:input_quote {
 
 function __smartinput:widget:backward_delete_char {
   local left right matched_pair=false
-  # Iterate over the rules to find if there's a matched pair for deletion
   for rule in "${__smartinput_rules[@]}"; do
     if [[ "${#rule}" == 1 ]]; then
       left="$rule"
@@ -59,9 +53,6 @@ function __smartinput:widget:backward_delete_char {
     elif [[ "${#rule}" == 4 ]]; then
       left="${rule:0:1}"
       right="${rule:2:1}"
-    else
-      # Not a valid rule
-      continue
     fi
 
     if [[ "$LBUFFER[-1]" == "$left" && "$RBUFFER[1]" == "$right" ]]; then
@@ -78,14 +69,20 @@ function __smartinput:widget:backward_delete_char {
   fi
 }
 
+# Register widgets
 zle -N __smartinput:widget:input_left_bracket
 zle -N __smartinput:widget:input_right_bracket
 zle -N __smartinput:widget:input_quote
 zle -N __smartinput:widget:backward_delete_char
-bindkey "^H" __smartinput:widget:backward_delete_char
-bindkey "^?" __smartinput:widget:backward_delete_char
 
-local rule
+# Bind keys to widgets
+bindkey '^H' __smartinput:widget:backward_delete_char
+bindkey '^?' __smartinput:widget:backward_delete_char
+bindkey '[' __smartinput:widget:input_left_bracket
+bindkey '{' __smartinput:widget:input_left_bracket
+bindkey '(' __smartinput:widget:input_left_bracket
+
+# Dynamically bind keys based on rules
 for rule in "${__smartinput_rules[@]}"; do
   if [[ "${#rule}" == 1 ]]; then
     bindkey "$rule" __smartinput:widget:input_quote
